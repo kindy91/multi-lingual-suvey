00733190a1f63c134fcaef67ddd16e6d00a06e15
Abdullah <abdullah.kindy@gmail.com>
2026-06-15 15:14:14 +0200

    final state

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { surveyLocalization } from 'survey-core';
+import { setupLocale, surveyLocalization } from 'survey-core';
 import { SurveyCreatorModule } from 'survey-creator-angular';
 import { SurveyCreatorModel } from "survey-creator-core";
 import 'survey-core/survey.i18n';
@@ -13,10 +13,15 @@ import 'survey-creator-core/survey-creator-core.i18n';
 })
 export class App {
   surveyCreatorModel?: SurveyCreatorModel;
-  ngOnInit() {
-    surveyLocalization.defaultLocale = 'en';
-    surveyLocalization.supportedLocales = ['en', 'de', 'fr', 'es', 'it'];
 
+  constructor() {
+    this.setupCustomLocaleAsDefault();
+
+    surveyLocalization.supportedLocales = ['en', 'de', 'fr', 'es', 'it'];
+  }
+
+  ngOnInit() {
+    // Init the creator model
     const creator = new SurveyCreatorModel({
       showSurveyHeader: false,
       showTranslationTab: true,
@@ -27,12 +32,39 @@ export class App {
       questionTypes: ["boolean", 'text', 'dropdown', 'checkbox', 'radiogroup', 'file'],
     });
 
-    creator.survey.locale = 'de';
 
+    // Enable the machine translation and provide a mock callback value
     creator.onMachineTranslate.add((_, options) => {
       options.callback(options.strings.map((str) => (`"${str}" from ${options.fromLocale} to  ${options.toLocale}`)));
     });
 
+    // Default JSON
+    creator.JSON = {
+      "elements": [
+        {
+          "type": "boolean",
+          "name": "question1",
+          "title": {
+            "de": "Magst du Fußball?"
+          }
+        }
+      ]
+    }
+    // Survey Locale is Gemran
+    creator.survey.locale = 'de';
+
     this.surveyCreatorModel = creator;
+  }
+
+  private setupCustomLocaleAsDefault() {
+    setupLocale({
+      localeCode: "customlocale",   // A short code used as a locale identifier (for example, "en", "de", "fr")
+      strings: {}, // An object with custom translations
+      nativeName: "Custom Locale",  // The locale name in native language
+      englishName: "Custom Locale", // The locale name in English 
+      rtl: false                    // A flag that indicates whether the language is right-to-left
+    });
+
+    surveyLocalization.defaultLocale = 'customlocale';
   }
 }
